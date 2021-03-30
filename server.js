8fdfde8d5cebc03104b5dc435f36a4b094ba8c0f
Jasmine Kay <jasminekay505@gmail.com>
2021-03-29 19:02:52 -0700

    Setting up server, used mini project code as guide

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const express = require('express');
+
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const routes = require('./controllers');
@@ -15,7 +16,7 @@ const PORT = process.env.PORT || 3001;
 const hbs = exphbs.create({ helpers });
 
 const sess = {
-  secret: 'Secret',
+  secret: 'Super Secret Secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
