424110a3813093480eb4631bde4d4bc6ce3bda4e
Jasmine Kay <jasminekay505@gmail.com>
2021-04-02 13:11:10 -0700

    Setting up login and sign up logic

---
+++ public/js/signup.js
@@ -0,0 +1,28 @@
+const signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    // Collect values from the sign up form
+    const name = document.querySelector('#name-signup').value.trim();
+    const email = document.querySelector('#email-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
+
+    if (name && email && password) {
+        // Send a POST request to the API endpoint
+        const response = await fetch('/api/users', {
+            method: 'POST',
+            body: JSON.stringify({ name, email, password }),
+            headers: { 'Content-Type': 'application/json' },
+        });
+
+        if (response.ok) {
+            // If successful, redirect the browser to the dashboard page
+            document.location.replace('/dashboard');
+        } else {
+            alert(response.statusText);
+        }
+    }
+};
+
+document
+    .querySelector('.signup-form')
+    .addEventListener('submit', signupFormHandler);
