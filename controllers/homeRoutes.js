e0da02e152eeea0f50bcd4d28c45a81f0124c7b6
Jasmine Kay <jasminekay505@gmail.com>
2021-04-01 19:53:00 -0700

    Settign up homeRoutes to show all posts

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,50 @@
 const seqeulize = require('../config/connection');
-const { Post, User, Comment } = require('../models');
 const router = require('express').Router();
+const { Post, User, Comment } = require('../models');
 
+router.get('/', (req, res) => {
+    Post.findAll({
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'date_created',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'content',
+                    'date_created',
+                    'user_id',
+                    'post_id'
+                ],
+                include: {
+                    model: User,
+                    attributes: [
+                        'username'
+                    ]
+                },
+            },
+            {
+                model: User,
+                attributes: [
+                    'username'
+                ],
+            },
+        ],
+    })
+        .then(dbPostData => {
+            const posts = dbPostData.map(post => post.get({ plain: true }));
+            res.render('homepage', {
+                posts,
+                loggedIn: req.session.loggedIn
+            });
+        })
+        .catch(err => {
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;
