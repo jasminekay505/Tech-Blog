2c4a20615ce213cdcc4d01a0e6c4e9302dee5714
Jasmine Kay <jasminekay505@gmail.com>
2021-04-02 14:45:09 -0700

    Adding comments and cleaning up

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,10 +1,13 @@
+//Importing dependencies
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const postRoutes = require('./postRoutes');
 const commentRoutes = require('./commentRoutes');
 
+//Setting up routes
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
 router.use('/comments', commentRoutes);
 
+//Export 
 module.exports = router;
