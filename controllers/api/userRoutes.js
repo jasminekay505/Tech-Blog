54535e7ce7f3cacc9b4eee6bf68f7a4f1c3eb446
Jasmine Kay <jasminekay505@gmail.com>
2021-04-02 15:06:44 -0700

    Adding comments and cleaning up

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,6 +1,8 @@
+//Import dependencies
 const router = require('express').Router();
 const { User } = require('../../models');
 
+//Create a new user and log them in
 router.post('/', async (req, res) => {
     try {
         const userData = await User.create(req.body);
@@ -11,28 +13,26 @@ router.post('/', async (req, res) => {
 
             res.status(200).json(userData);
         });
+
     } catch (err) {
         res.status(400).json(err);
     }
 });
 
+//Login route
 router.post('/login', async (req, res) => {
     try {
-        const userData = await User.findOne({ where: { email: req.body.email } });
+        const userData = await User.findOne({ where: { username: req.body.username } });
 
         if (!userData) {
-            res
-                .status(400)
-                .json({ message: 'Incorrect email or password, please try again' });
+            res.status(400).json({ message: 'Incorrect username or password, please try again' });
             return;
         }
 
         const validPassword = await userData.checkPassword(req.body.password);
 
         if (!validPassword) {
-            res
-                .status(400)
-                .json({ message: 'Incorrect email or password, please try again' });
+            res.status(400).json({ message: 'Incorrect email or password, please try again' });
             return;
         }
 
@@ -48,11 +48,13 @@ router.post('/login', async (req, res) => {
     }
 });
 
+//Logout route
 router.post('/logout', (req, res) => {
     if (req.session.logged_in) {
         req.session.destroy(() => {
             res.status(204).end();
         });
+
     } else {
         res.status(404).end();
     }
