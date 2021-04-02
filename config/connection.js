af77cb6948c5b1b639853e9bf184720020a8b48b
Jasmine Kay <jasminekay505@gmail.com>
2021-04-02 14:33:54 -0700

    Adding comments and cleaning up

--- config/connection.js
+++ config/connection.js
@@ -2,9 +2,8 @@
 const Sequelize = require('sequelize');
 require('dotenv').config();
 
-let sequelize;
-
 //Set up sequelize
+let sequelize;
 if (process.env.JAWSDB_URL) {
   sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
