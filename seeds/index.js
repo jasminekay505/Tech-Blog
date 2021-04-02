060390e114d1e6bb6ddd2f9b9d7311a832295d1a
Jasmine Kay <jasminekay505@gmail.com>
2021-04-01 21:11:49 -0700

    fixing typos

--- seeds/index.js
+++ seeds/index.js
@@ -8,13 +8,13 @@ const seedAll = async () => {
   await sequelize.sync({ force: true });
   console.log('\n----- DATABASE SYNCED -----\n');
   await seedPosts();
-  console.log('\n----- CATEGORIES SEEDED -----\n');
+  console.log('\n----- POSTS SEEDED -----\n');
 
   await seedUsers();
-  console.log('\n----- PRODUCTS SEEDED -----\n');
+  console.log('\n----- USERS SEEDED -----\n');
 
   await seedComments();
-  console.log('\n----- TAGS SEEDED -----\n');
+  console.log('\n----- COMMENTS SEEDED -----\n');
 
   process.exit(0);
 };
