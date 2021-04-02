613d12c0b2cd539e7c690cd6c4af7018ac6e2be3
Jasmine Kay <jasminekay505@gmail.com>
2021-04-01 20:02:33 -0700

    Added login, signup and single post routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -47,4 +47,72 @@ router.get('/', (req, res) => {
         });
 });
 
+router.get('/login', (req,res) => { 
+    if(req.session.loggedIn) { 
+        res.redirect('/');
+        return;
+    }
+    res.render('login');
+});
+
+router.get('/signup', (req,res) => {
+    if(req.session.loggedIn) { 
+        res.redirect('/');
+        return;
+    }
+    res.render('signup');
+});
+
+router.get('/post/:id', (req,res) => { 
+    Post.findOne({
+        where: { 
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'title',
+            'content',
+            'date_created',
+        ],
+        include: [
+            {
+                model: Comment,
+                attributes: [
+                    'id',
+                    'content',
+                    'date_created',
+                    'user_id',
+                    'post_id'
+                ],
+                include: {
+                    model: User,
+                    attributes: [
+                        'username'
+                    ]
+                },
+            },
+            {
+                model: User,
+                attributes: [
+                    'username'
+                ],
+            },
+        ],
+    })
+    .then(dbPostData => { 
+        if(!dbPostData) { 
+            res.status(404).json({ message: 'No post found with this id!'});
+            return;
+        }
+        const post = dbPostData.get({ plain: true });
+        res.render('single-post', {
+            post,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => { 
+        res.status(500).json(err);
+    });
+});
+
 module.exports = router;
