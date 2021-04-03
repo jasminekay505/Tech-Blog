80ef5aeb39579341bbbd63e85efc7efc351cb1d9
Jasmine Kay <jasminekay505@gmail.com>
2021-04-03 14:21:51 -0700

    Trying to get login to work

--- public/js/signup.js
+++ public/js/signup.js
@@ -3,20 +3,21 @@ const signupFormHandler = async (event) => {
     event.preventDefault();
 
     // Collect values from the sign up form
-    const name = document.querySelector('#name-signup').value.trim();
+    const username = document.querySelector('#name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
-    if (name && email && password) {
+    if (username && email && password) {
         // Send a POST request to the API endpoint
         const response = await fetch('/api/users', {
             method: 'POST',
-            body: JSON.stringify({ name, email, password }),
+            body: JSON.stringify({ username, email, password }),
             headers: { 'Content-Type': 'application/json' },
         });
 
         if (response.ok) {
             // If successful, redirect the browser to the dashboard page
+            console.log('success! signedup!')
             document.location.replace('/dashboard');
         } else {
             alert(response.statusText);
