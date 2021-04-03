3bf832e564652ccb94c8e648aea75d70c81734ea
Jasmine Kay <jasminekay505@gmail.com>
2021-04-02 17:18:27 -0700

    Adding comments and cleaning up

--- controllers/dashboardRoutes.js
+++ controllers/dashboardRoutes.js
@@ -1,155 +1,30 @@
-const seqeulize = require('../config/connection');
+//Import dependencies
 const router = require('express').Router();
 const { Post, User, Comment } = require('../models');
 const withAuth = require('../utils/auth')
 
-router.get('/', withAuth, (req, res) => {
-    Post.findAll({
-        where: {
-            user_id: req.session.user_id
-        },
-        attributes: [
-            'id',
-            'title',
-            'content',
-            'date_created',
-        ],
-        include: [
-            {
-                model: Comment,
-                attributes: [
-                    'id',
-                    'content',
-                    'date_created',
-                    'user_id',
-                    'post_id'
-                ],
-                include: {
+//Show all posts that were created by the user that is currently logged in
+router.get('/', withAuth, async (req, res) => {
+    try {
+        const postData = await Post.findAll({
+            include:
+                [{
                     model: User,
-                    attributes: [
-                        'username'
-                    ]
-                },
-            },
-            {
-                model: User,
-                attributes: [
-                    'username'
-                ],
-            },
-        ],
-    })
-        .then(dbPostData => {
-            const posts = dbPostData.map(post => post.get({ plain: true }));
-            res.render('dashboard', {
-                posts,
-                loggedIn: true
-            });
-        })
-        .catch(err => {
-            res.status(500).json(err);
+                    attributes: ['username']
+                }],
+            where: {
+                user_id: req.session.user_id
+            }
         });
-});
+        const posts = postData.map(post => post.get({ plain: true }));
 
-router.get('/edit/:id', withAuth, (req,res) => { 
-    Post.findOne({
-        where: { 
-            id: req.params.id
-        },
-        attributes: [
-            'id',
-            'title',
-            'content',
-            'date_created',
-        ],
-        include: [
-            {
-                model: Comment,
-                attributes: [
-                    'id',
-                    'content',
-                    'date_created',
-                    'user_id',
-                    'post_id'
-                ],
-                include: {
-                    model: User,
-                    attributes: [
-                        'username'
-                    ]
-                },
-            },
-            {
-                model: User,
-                attributes: [
-                    'username'
-                ],
-            },
-        ],
-    })
-    .then(dbPostData => { 
-        if(!dbPostData) { 
-            res.status(404).json({ message: 'No post found with this id!'});
-            return;
-        }
-        const post = dbPostData.get({ plain: true });
-        res.render('edit-post', {
-            post,
-            loggedIn: true
+        res.render('dashboard', {
+            posts, loggedIn: req.session.loggedIn
         });
-    })
-    .catch(err => { 
+
+    } catch (err) {
         res.status(500).json(err);
-    });
+    }
 });
-
-router.get('/create/', withAuth, (req,res) => { 
-    Post.findAll({
-        where: { 
-            user_id: req.session.user_id
-        },
-        attributes: [
-            'id',
-            'title',
-            'content',
-            'date_created',
-        ],
-        include: [
-            {
-                model: Comment,
-                attributes: [
-                    'id',
-                    'content',
-                    'date_created',
-                    'user_id',
-                    'post_id'
-                ],
-                include: {
-                    model: User,
-                    attributes: [
-                        'username'
-                    ]
-                },
-            },
-            {
-                model: User,
-                attributes: [
-                    'username'
-                ],
-            },
-        ],
-    })
-    .then(dbPostData => { 
-        const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('create-post', {
-            posts,
-            loggedIn: true
-        });
-    })
-    .catch(err => { 
-        res.status(500).json(err);
-    });
-});
-
 
 module.exports = router;
