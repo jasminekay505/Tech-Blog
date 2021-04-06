c4e7782cc9543c449cfe977e0135d92bdfd10f21
Jasmine Kay <jasminekay505@gmail.com>
2021-04-06 14:25:46 -0700

    trying to fix single post route

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,20 +1,23 @@
-// Import dependencies
+//Import dependencies
 const router = require('express').Router();
-const { Post, User, Comment } = require('../models');
+const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
-//Homepage - show all posts
+
+//Show all posts on homepage
 router.get('/', async (req, res) => {
     try {
         const postData = await Post.findAll({
             include: [
                 {
                     model: User,
-                    attributes: ['username']
-                }
-            ]
+                    attributes: ['username'],
+                },
+            ],
         });
-        const posts = postData.map(post => post.get({ plain: true }));
+
+        const posts = postData.map((post) => post.get({ plain: true }));
+
         res.render('homepage', {
             posts,
             logged_in: req.session.logged_in
@@ -24,28 +27,13 @@ router.get('/', async (req, res) => {
     }
 });
 
-//Login page
-router.get('/login', (req, res) => {
-    if (req.session.logged_in) {
-        res.redirect('/dashboard');
-        return;
-    }
-    res.render('login');
-});
-
-//Signup page
-router.get('/signup', (req, res) => {
-    if (req.session.logged_in) {
-        res.redirect('/dashboard');
-        return;
-    }
-    res.render('signup');
-});
-
-//Single Post
+//Single page view with comments
 router.get('/post/:id', async (req, res) => {
     try {
-        const postData = await Post.findbyPk(req.params.id, {
+        const postData = await Post.findOne({
+            where: { 
+                id: req.params.id
+            },
             include: [
                 {
                     model: Comment,
@@ -73,6 +61,7 @@ router.get('/post/:id', async (req, res) => {
     }
 });
 
+//Dashboard view for logged_in User
 router.get('/dashboard', withAuth, async (req, res) => {
     try {
         // Find the logged in user based on the session ID
@@ -90,6 +79,28 @@ router.get('/dashboard', withAuth, async (req, res) => {
     } catch (err) {
         res.status(500).json(err);
     }
+});
+
+//Login page
+router.get('/login', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('login');
+});
+
+//Sign up page
+router.get('/signup', (req, res) => {
+    // If the user is already logged in, redirect the request to another route
+    if (req.session.logged_in) {
+        res.redirect('/dashboard');
+        return;
+    }
+
+    res.render('signup');
 });
 
 module.exports = router;
